5f85e404f6e7123648a04bacce8a652aae863746
Rahulbobadi <rahul.bobadi@gmail.com>
2026-06-11 19:11:59 +0530

    Moved SSH keys

--- terraform/vm-infra/main.tf
+++ terraform/vm-infra/main.tf
@@ -111,8 +111,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
 source_image_reference {
   publisher = "Canonical"
-  offer     = "ubuntu-24_04-lts"
-  sku       = "server-arm64"
+  offer     = "0001-com-ubuntu-server-jammy"
+  sku       = "22_04-lts-gen2"
   version   = "latest"
 }
 
